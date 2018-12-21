1ae065a57857e2630d96cd7544ba2beb7413f8f2
Patrick <patrick.angelis@hotmail.com>
2018-12-21 20:15:26 -0300

    Atualização erro na configuração ALLOWED_HOSTS

--- eventex/settings.py
+++ eventex/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 #ALLOWED_HOSTS = ['.127.0.0.1']
-ALLOWED_HOSTS = config('ALLOWED_HOST', default=[], cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
 
 
 # Application definition
